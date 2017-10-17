edd339df6babb30ee0476b619d37a986a6e1d2e4
Kyru <ferraniusd@gmail.com>
2017-10-17 13:06:30 +0200

    Autosave

---
+++ Scripts/transform_md.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/Users/ferranius/Desktop/uniArchives/Scripts
+quiver_to_markdown /Users/ferranius/Desktop/uniArchives/Scripts/Quiver.qvlibrary /Users/ferranius/Desktop/uniArchives/
+echo "Transformacion completa"
